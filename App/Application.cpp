9a068d34f173a979ee152f579be17e31c9777c20
pjastrze <p.jastrzebski83@gmail.com>
2018-11-07 14:59:04 +0100

    Added Base App Visaul Solution

---
+++ App/Application.cpp
@@ -0,0 +1,6 @@
+#include "TestClass.h"
+
+int main()
+{
+    return 0;
+}
---
+++ App/TestClass.cpp
@@ -0,0 +1,15 @@
+#include "TestClass.h"
+#include <iostream>
+
+using namespace std;
+
+TestClass::TestClass()
+{
+    std::cout << "Default Test Class Constructor\n";
+}
+
+
+TestClass::~TestClass()
+{
+    std::cout << "Default Test Class Destructor\n";
+}
