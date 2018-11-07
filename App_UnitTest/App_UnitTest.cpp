eda49a4c4d4d38e7a2b80626da846481737a4ef7
pjastrze <p.jastrzebski83@gmail.com>
2018-11-07 16:32:41 +0100

    Added Configured GTest & GMock Projects

---
+++ App_UnitTest/App_UnitTest.cpp
@@ -0,0 +1,15 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "TestClass.h"
+
+TEST(GTestTest, GoogleTestFrameworkInitTest)
+{
+
+}
+
+TEST(TestClassTest, TestClassInitTest)
+{
+    TestClass test;
+}
+
